26c9013a60db8da9e25bdf5ebfa094ddb9b75619
anchenayres <21100284@virtualwindow.co.za>
2023-11-05 14:51:21 +0200

    not showcasing right

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import os
 import joblib
+import pandas as pd
 
 # Creating our app
 app = FastAPI()
@@ -21,6 +22,39 @@ app.add_middleware(
     allow_methods=methods,
     allow_headers=headers
 )
+
+
+# Create a prediction endpoint
+@app.get("/predict/")
+def predict_shadow(
+    year: int, 
+    feb_temp: float, 
+    mar_temp: float, 
+    northeast_feb: float = 0.0,  # Provide default values
+    midwest_feb: float = 0.0,
+    penns_feb: float = 0.0,
+    northeast_mar: float = 0.0,
+    midwest_mar: float = 0.0,
+    penns_mar: float = 0.0
+):
+    
+    # Prepare new data for prediction
+    new_data = pd.DataFrame({
+        'Year': [year],
+        'February Average Temperature': [feb_temp],
+        'February Average Temperature (Northeast)': [northeast_feb],
+        'February Average Temperature (Midwest)': [midwest_feb],
+        'February Average Temperature (Pennsylvania)': [penns_feb],
+        'March Average Temperature': [mar_temp],
+        'March Average Temperature (Northeast)': [northeast_mar],
+        'March Average Temperature (Midwest)': [midwest_mar],
+        'March Average Temperature (Pennsylvania)': [penns_mar]
+    })
+
+    # Make predictions
+    predicted_class = groundHog_model.predict(new_data)
+
+    return {"Predicted Shadow": predicted_class[0]}
 
 # Implement default API endpoint
 @app.get("/")
